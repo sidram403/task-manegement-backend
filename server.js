bddaf489c8ee456eadf64c5aafea692fc3a855e7
sidram403 <siddharthpatil9108@gmail.com>
2025-02-26 22:10:21 +0530

    updated in origin

--- server.js
+++ server.js
@@ -14,19 +14,19 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://task-management-woad-three.vercel.app",
+    origin: "https://task-management-frontend-rho-flax.vercel.app",
     // origin: "http://localhost:5173",
 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://task-management-woad-three.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.sendStatus(200);
-});
+// app.options("*", (req, res) => {
+//   res.header("Access-Control-Allow-Origin", "https://task-management-woad-three.vercel.app");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.sendStatus(200);
+// });
 app.use(express.json()); // ✅ Keep CORS before routes
 app.use(express.urlencoded({ extended: true }));
 
